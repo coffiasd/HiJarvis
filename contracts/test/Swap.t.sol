5247729350c6c5cbd050116d4f90e6db4f57c5b6
coffiasd <759291502@qq.com>
2023-06-20 13:20:15 +0800

    add:op

--- contracts/test/Swap.t.sol
+++ contracts/test/Swap.t.sol
@@ -5,7 +5,7 @@ import "forge-std/Test.sol";
 
 import {console} from "forge-std/console.sol";
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
 
 contract Swap is Test {
     //set 2 erc20 tokens.
