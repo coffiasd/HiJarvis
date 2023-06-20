bf972fb396aa9b13217c0c67d5d7d66d0816cd03
coffiasd <759291502@qq.com>
2023-06-20 13:16:37 +0800

    init

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
             <p className="py-6 text-xl font-normal leading-normal mt-0 mb-2">Digital-ticket-stream is a platform that allows users to create paid live streaming channels. Users can create their own NFTs on the platform or import an existing NFT to charge custom fees to viewers</p>
           </div>
 
-          <div class="flex flex-col gap-6 mt-20">
+          {/* <div class="flex flex-col gap-6 mt-20">
             <div class="not-prose grid grid-cols-3 gap-x-6 gap-y-12 sm:grid-cols-2 lg:grid-cols-4">
               <a class="card card-compact hover:bg-base-400 transition-all duration-400 hover:-translate-y-1" href=""><figure class="px-4 pt-4"><FaPlusSquare size="6rem" /></figure> <div class="card-body"><h2 class="text-center text-lg font-bold">Create Room</h2></div></a>
 
@@ -42,7 +42,7 @@ export default function Home() {
 
 
             </div>
-          </div>
+          </div> */}
 
         </div>
 
