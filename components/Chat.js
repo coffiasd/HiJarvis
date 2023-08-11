6ee11deae5f30ee9eb26099de7edcc9139d1b9c5
coffiasd <759291502@qq.com>
2023-08-11 19:29:43 +0800

    fix

--- components/Chat.js
+++ components/Chat.js
@@ -279,7 +279,7 @@ export default function Chat() {
 
 
     const sendMessage = (message) => {
-        const url = '/api/generate';
+        const url = '/api/chat';
 
         console.log(message);
 
@@ -308,8 +308,7 @@ export default function Chat() {
         setIsLoading(true);
 
         axios.post(url, data).then((response) => {
-            console.log("Res:",response);
-            return ;
+       
             if (response.data.choices[0].finish_reason == "function_call") {
                 console.log(response.data.choices[0].message.function_call.name + "(" + response.data.choices[0].message.function_call.arguments + ")");
                 eval(response.data.choices[0].message.function_call.name + "(" + response.data.choices[0].message.function_call.arguments + ")");
